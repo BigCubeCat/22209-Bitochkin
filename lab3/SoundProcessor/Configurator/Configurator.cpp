014a5b7b44edd92dbd924c4442d5eb6d4de1ff93
BigCubeCat <bigcubecat@yandex.ru>
2023-10-18 23:00:22 +0700

    feat(parsing): config parsing created
    converter params not finished

--- lab3/SoundProcessor/Configurator/Configurator.cpp
+++ lab3/SoundProcessor/Configurator/Configurator.cpp
@@ -1,7 +1,7 @@
 #include "Configurator.h"
 
-#include <iostream>
 #include <fstream>
+#include <sstream>
 
 Configurator::~Configurator() = default;
 
@@ -13,12 +13,56 @@ bool Configurator::hasErrors() const {
     return errorsOccurred;
 }
 
-void Configurator::run() {
+void Configurator::parse() {
     std::ifstream nameFileout;
     nameFileout.open(fileName);
     std::string line;
-    while(std::getline(nameFileout, line))
-    {
-        std::cout << "line:" << line << std::endl;
+    while (std::getline(nameFileout, line)) {
+        parseConfigLine(line);
     }
 }
+
+void Configurator::run() {
+    parse();
+    if (!errorsOccurred) {
+
+    }
+}
+
+void Configurator::parseConfigLine(const std::string &line) {
+    auto cmd = splitLine(line);
+    if (cmd.empty()) {
+        return;
+    }
+    if (cmd[0] == "#") {
+        return;
+    }
+    if (!isConvertorName(cmd[0])) {
+        errorsOccurred = true;
+        errorMessage += "Invalid command: " + line + "\n";
+    }
+}
+
+std::vector<std::string> Configurator::splitLine(const std::string &line) {
+    std::vector<std::string> result;
+    std::string s;
+    std::stringstream ss(line);
+    std::vector<std::string> v;
+
+    while (getline(ss, s, ' ')) {
+        result.push_back(s);
+    }
+    return result;
+}
+
+bool Configurator::isConvertorName(const std::string &name) {
+    return name == "mix" || name == "mute" || name == "reverb";
+}
+
+bool Configurator::commandIsValid(const std::vector<std::string> &cmd) {
+    if (cmd[0] == "mix") {
+
+    }
+}
+
+
--- lab3/SoundProcessor/Configurator/Configurator.h
+++ lab3/SoundProcessor/Configurator/Configurator.h
@@ -2,6 +2,7 @@
 #define SOUNDPROCESSOR_CONFIGURATOR_H
 
 #include <string>
+#include <vector>
 
 class Configurator {
 public:
@@ -9,13 +10,40 @@ public:
 
     ~Configurator();
 
+    /*
+     * parse();
+     * Read line by line.
+     * Calls parseConfigLine(line)
+     */
+    void parse();
+
+    /*
+     * run();
+     * call parse();
+     * run all convertors and write result
+     */
     void run();
+
+    /*
+     * parseConfigLine(line);
+     * Reads line and create Converter
+     */
+    void parseConfigLine(const std::string &line);
 
     bool hasErrors() const;
 
 private:
     std::string fileName;
+
+    std::string errorMessage;
     bool errorsOccurred = false;
+
+    static std::vector<std::string> splitLine(const std::string &line);
+
+    static bool isConvertorName(const std::string &name);
+
+    static bool commandIsValid(const std::vector<std::string> &cmd);
+
 };
 
 #endif
