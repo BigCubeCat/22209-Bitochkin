0baa38d17f89b1cf7bffcd2dbeb28fa0d138d762
BigCubeCat <bigcubecat@yandex.ru>
2023-12-13 00:50:58 +0700

    fix(factory): create lambda functions

--- lab3/SoundProcessor/lib/Converter/include/converterFactory.h
+++ lab3/SoundProcessor/lib/Converter/include/converterFactory.h
@@ -6,6 +6,9 @@
 #include <stdexcept>
 #include <memory>
 #include <string>
+#include <map>
+#include <functional>
+
 #include "mute.h"
 #include "mix.h"
 #include "noise.h"
@@ -14,7 +17,7 @@
 
 namespace converterFactory {
 
-    class NotFoundConverter: public std::invalid_argument{
+    class NotFoundConverter : public std::invalid_argument {
     public:
         explicit NotFoundConverter(const std::string &converterName);
     };
@@ -25,11 +28,22 @@ namespace converterFactory {
     const std::string CUT = "cut";
     const std::string CROP = "crop";
 
+
     using ConverterPointer = std::unique_ptr<converter::Converter>;
+
+    std::map<std::string, std::function<ConverterPointer (const std::vector<std::string>&)>> converterMap = {
+            {"mix", [](const std::vector<std::string> &params) {return std::make_unique<mix::Mix>(params);}},
+            {"mute", [](const std::vector<std::string> &params) {return std::make_unique<mute::Mute>(params);}},
+            {"crop", [](const std::vector<std::string> &params) {return std::make_unique<crop::Crop>(params);}},
+            {"cut", [](const std::vector<std::string> &params) {return std::make_unique<cut::Cut>(params);}},
+            {"noise", [](const std::vector<std::string> &params) {return std::make_unique<noise::Noise>(params);}}
+    };
 
     class ConverterFactory {
     public:
-        ConverterPointer createConverter(const std::vector<std::string> parameters);
+        ConverterPointer createConverter(const std::vector<std::string> parameters) {
+            return converterMap[parameters[0]](parameters);
+        };
     };
 }
 #endif
